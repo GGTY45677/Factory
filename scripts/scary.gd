b5bee3d9b472f209230024ab7e9e110b6254a90e
Sibusiso <sibusisoadrian2009@gmail.com>
2026-06-28 17:27:23 +0200

    TriggerZone Update

--- scripts/scary.gd
+++ scripts/scary.gd
@@ -1,5 +1,8 @@
 extends Sprite3D
 
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
 func _on_player_entered() -> void:
+	animation.play("Scare_ani")
+	await animation.animation_finished
 	queue_free()
-	print("disappeared")
